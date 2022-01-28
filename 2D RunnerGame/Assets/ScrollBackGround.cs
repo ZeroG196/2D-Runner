230674e2ef913de548ff307340742ec954d661c0
ZeroG196 <92901461+ZeroG196@users.noreply.github.com>
2022-01-28 16:32:41 +0900

    2D start

---
+++ 2D RunnerGame/Assets/ScrollBackGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackGround : MonoBehaviour
+{
+    public float speed;
+    public Transform[] backgrounds;
+
+    float leftPosX = 0f;
+    float rightPosX = 0f;
+    float xScreenHalfSize;
+    float yScreenHalfSize;
+
+    void Start()
+    {
+        xScreenHalfSize = Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
